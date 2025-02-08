805c76a663afad114cdc686714033fa907fac329
sethumdilnuka <dilnuka1234@gmail.com>
2025-02-08 16:54:16 +0530

    commit 1

--- src/main/java/edu/ijse/layerd/controller/CustomerFormController.java
+++ src/main/java/edu/ijse/layerd/controller/CustomerFormController.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.function.Predicate;
 
 
-
 public class CustomerFormController {
 
     CustomerBO customerBO = (CustomerBO) BOFactory.getBoFactory().getTypes(BOFactory.BOTypes.Customer);
